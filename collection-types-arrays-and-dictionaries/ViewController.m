046cb82c31c9566c8fdbeee742379183d6006922
張書涵 <zhangshuhan@zhangshuhans-macbook-pro.local>
2018-12-26 11:47:20 +0800

    collection-types-arrays-and-dictionaries

--- collection-types-arrays-and-dictionaries/ViewController.m
+++ collection-types-arrays-and-dictionaries/ViewController.m
@@ -17,6 +17,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSMutableDictionary *bostionDict = [[NSMutableDictionary alloc] initWithObjectsAndKeys:@"Boston",@"City", @"USA",@"Country", nil];
+    
+   // NSDictionary *bostionDict = @{@"City":@"Bostion",@"Country":@"USA"};
+    
+    //NSMutableDictionary *mutBostion = [[NSMutableDictionary alloc] initWithDictionary:bostionDict];
+    
+
+   // NSArray *beantown = [bostionDict valueForKey:@"City"];
+    
+    [bostionDict setValue:@"Brazil" forKey:@"Country"];
+    
+    [bostionDict addEntriesFromDictionary:@{@"Population":@(656000)}];
+     
+    NSMutableDictionary *atlDict = [[NSMutableDictionary alloc] initWithObjectsAndKeys:@"Atlantis", @"City",@"Spain",@"Country",@(20000), @"Population", nil];
+    
+    NSArray *cities = @[bostionDict,atlDict];
+    
+    int i = [cities count];
+    
+    int j = cities.count;
+    
+    NSMutableDictionary *sunkenCity = [cities objectAtIndex:1];
+    
+    NSString *sunkenCityNAme = [[cities objectAtIndex:1]
+                                valueForKey:@"City"];
+    
+    int pop = [[[cities objectAtIndex:1]
+                valueForKey:@"Population"] intValue];
+    
+    
+    NSMutableArray *nameArray = [[NSMutableArray alloc]initWithObjects:@"Passan",@"Grabe",@"Alice",nil];
+    
+    [nameArray addObject:@"Nick"];
+    
+    [nameArray removeObjectAtIndex:2];
+    
+    [nameArray replaceObjectAtIndex:1 withObject:@"Annie"];
+    
+    NSArray <NSString *> * stringArray;
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
 
